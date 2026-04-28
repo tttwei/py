eaa61b1c35da482f48d58819c081c873042a7191
tttwei <2119515837@qq.com>
2026-04-28 21:20:26 +0800

    Changes0428

--- click/click_press.py
+++ click/click_press.py
@@ -15,13 +15,23 @@ def to_click1():
     pyautogui.click(x+172, y-108,duration=0.3)
     pyautogui.click(1362,695,duration=0.1)
 
+def to_click2():
+    print('触发了')
+    x, y = pyautogui.locateCenterOnScreen('./img/qianniu2.png',region= (597, 1142,703,58), confidence=0.9)
+    pyautogui.click(x, y,duration=0.1)
+    # print(x,y)
+
+    pyautogui.click(x+172, y-108,duration=0.2)
+    pyautogui.moveTo(707, 275, duration=0.1)
 # keyboard.add_hotkey('F4',to_click)
 
 def on_f4():
     threading.Thread(target=to_click1).start()
-
+def on_f3():
+    threading.Thread(target=to_click2).start()
 h = keyboard.GlobalHotKeys({
-    '<f4>': on_f4
+    '<f4>': on_f4,
+    '<f3>': on_f3
 })
 
 h.start()
