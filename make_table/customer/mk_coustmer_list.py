76d8a128b3c6ba5cdbe50ef620875f329eca6457
tttwei <2119515837@qq.com>
2026-05-31 13:52:36 +0800

    Changes0531

--- make_table/customer/mk_coustmer_list.py
+++ make_table/customer/mk_coustmer_list.py
@@ -1,14 +1,22 @@
+total = 0
+shouqian = 0
+
+
 f = open('c.txt', 'r', encoding='utf-8')
 readline = f.readline()
 while readline:
+    total += 1
     str_array = readline.replace("\n", "").split('，')
 
     # my_send_msg(str_array[0], str_array[1])
     print(str_array[0],end='')
     if '售前' in str_array[1]:
         print(f'(售前未成交，{str_array[2]})，',end='')
+        shouqian += 1
     elif '售后' in str_array[1]:
         print(f'({str_array[1]}，{str_array[2]})，',end='')
 
     # print(str_array[0],str_array[1])
     readline = f.readline()
+print()
+print(f'总数：{total}，售前：{shouqian}')
