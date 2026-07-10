79a65a09d8c96ab81cc604a56addaf0d2b75ee19
tttwei <2119515837@qq.com>
2026-07-10 15:19:39 +0800

    Changes0710 增加jdself

---
+++ make_table/jd_self/mk_jdself.py
@@ -0,0 +1,60 @@
+import json
+import time
+
+import pandas as pd
+import requests
+
+from application import request_header
+
+# f = open('a.json','r',encoding='utf-8')
+
+# data = json.load(f)
+
+my_json = request_header.jd_self_header
+
+my_url = 'https://aic.cbbs.tmall.com/aic-report/queryAicInventory'
+
+
+
+code_list = ['HUZ225','CAN2133','SHA2140']
+df3 = pd.DataFrame({
+    '仓库CODE': code_list,
+    '1062705212742':['']*3,
+    '1059959347160':['']*3,
+    '1061764569159':['']*3,
+    '1060771738685':['']*3,
+
+})
+w_list = [
+    '1062705212742',
+    '1059959347160',
+    '1061764569159',
+    '1060771738685'
+]
+for w_code in w_list:
+
+    print(f'开始运行，仓库代码：{w_code}')
+    time.sleep(3)
+    my_params = {
+        "hideZeroInventory": "1",
+        "pageIndex": "1",
+        "pageSize": "20",
+        "scItemIds": w_code
+    }
+
+    response = requests.get(
+        url=my_url,
+        headers=my_json,
+        params=my_params
+    )
+    print('网络状态码',response.status_code)
+    data = response.json()
+
+    for obj in data['data']['dataSource']:
+        ipm_num = obj['ipmInventory']['items'][0]['value'] + obj['ipmInventory']['items'][1]['value']
+        code_name = obj['storeInfo'].split('\n')[1].split(':')[1]
+        print(ipm_num)
+        print(code_name)
+        df3.loc[df3['仓库CODE'] == code_name, w_code] = ipm_num
+
+df3.to_excel('b2_total.xlsx')
